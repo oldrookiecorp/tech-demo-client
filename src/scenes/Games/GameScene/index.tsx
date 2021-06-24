825a16b6979b8ba3a092954dce37434ead63ffa6
seonghyeon <tjdgusdkf@gmail.com>
2021-06-25 08:46:38 +0900

    feat: 쿼리스트링, 게임 종료 처리

--- src/scenes/Games/GameScene/index.tsx
+++ src/scenes/Games/GameScene/index.tsx
@@ -30,19 +30,37 @@ const GameScene = ({ match }: RouteComponentProps<GameIdMatchParams>) => {
     setLoading(false);
   };
 
+  const gameEnd = () => {
+    alert('Game Ended!');
+  };
+
+  window.addEventListener('message', (e) => {
+    // 전달 된 데이터
+    console.log(e.data.functionName);
+
+    // 부모창의 함수 실행
+    if (e.data.functionName === 'gameEnd') {
+      setVisibility(true);
+      if (e.data.userIdx) {
+        alert(e.data.userIdx);
+      }
+    }
+  });
+
   const [visibility, setVisibility] = useState<boolean>(false);
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      setVisibility(true);
-    }, 5000);
-    return () => clearTimeout(timeout);
+    // const timeout = setTimeout(() => {
+    //   setVisibility(true);
+    // }, 5000);
+    // return () => clearTimeout(timeout);
   }, []);
 
   return (
     <>
       <iframe
-        src={`https://aframe.io/examples/showcase/anime-UI?user=${nickname}`}
+        // src={`https://aframe.io/examples/showcase/anime-UI?user=${nickname}`}
+        src={`https://0.0.0.0:8888/Normal?user=${nickname}`}
         className={cn('ifram__container')}
         onLoad={aframeLoad}
       />
