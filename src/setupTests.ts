77161f0a43b193b9825ca44e90d5e0d33eafbb53
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2021-06-25 17:32:02 +0900

    jest

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,3 +4,16 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 import 'jest-localstorage-mock';
+
+exports.module = {
+  verbose: true,
+  resetMocks: false,
+  setupFiles: ['jest-localstorage-mock'],
+  moduleNameMapper: {
+    '\\.(css|scss|less)$': 'identity-obj-proxy'
+  },
+  transform: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__test__/fileTransformer.js'
+  }
+};
