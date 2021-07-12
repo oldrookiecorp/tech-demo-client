d218073ad393806e72ebc6b18d9f4f54c3af400d
seonghyeon <tjdgusdkf@gmail.com>
2021-07-12 18:22:23 +0900

    feat: vr mode

--- src/scenes/Games/GameScene/index.tsx
+++ src/scenes/Games/GameScene/index.tsx
@@ -22,6 +22,17 @@ export interface GameIdMatchParams {
   game_id: string;
 }
 
+// declare global {
+//   interface HTMLIFrameElement {
+//     allowvr?: string;
+//   }
+// }
+declare var HTMLIFrameElement: {
+  // prototype: HTMLIFrameElement;
+  allowvr: string;
+  // new (): HTMLIFrameElement;
+};
+
 const GameScene = ({ match }: RouteComponentProps<GameIdMatchParams>) => {
   const { game_id } = match.params;
 
@@ -110,22 +121,19 @@ const GameScene = ({ match }: RouteComponentProps<GameIdMatchParams>) => {
     };
   });
 
-  useEffect(() => {
-    console.log('rankData : ', rankData);
-  }, [rankData]);
-
   return (
     <>
       {data && (
         <iframe
-          data-type="text/html"
-          src={`${data.aframeUrl}?gameId=${game_id}&user=${nickname}`}
+          // src={`${data.aframeUrl}?gameId=${game_id}&user=${nickname}`}
+          src={`https://192.168.219.180:8888?gameId=${game_id}&user=${nickname}`}
           className={cn('ifram__container')}
           onLoad={aframeLoad}
+          scrolling="no"
           allowFullScreen
-          data-mozAllowFullscreen="true"
-          data-webkitallowfullscreen="true"
-          data-allowvr="yes"
+          mozAllowFullscreen="true"
+          webkitallowfullscreen="true"
+          allowvr="yes"
           allow="vr; xr; accelerometer; magnetometer; gyroscope; webvr;webxr;"
         />
       )}
